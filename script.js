ed23f9c2d8d1e307d80bd2cf7f8756caf8eda6f0
Benjamin Rossario A <benrobotics74@gmail.com>
2024-07-17 01:05:44 +0530

    formatOutput fn

--- script.js
+++ script.js
@@ -40,35 +40,55 @@ const checkRegister = () => {
   );
 
   if (totalCid < changeDue) {
-    displayChangeDue.innerHTML = "<p>Status: INSUFFICIENT_FUNDS</p>";
+    displayChangeDue.innerHTML = "<p>Status: INSUFFICIENT_FUNDS1</p>";
     return;
   }
   if (totalCid === changeDue) {
     result.status = "CLOSED";
   }
+  console.log(reversedCid.length);
   for (let idx = 0; idx < reversedCid.length; idx++) {
-    if (changeDue > 0 && changeDue >= denominations[i]) {
-      count = 0;
+    console.log(1);
 
-      while (changeDue >= denominations[i]) {
-        reversedCid[i][1] -= denominations[i];
-        changeDue = changeDue - denominations;
-        changeDue = parseFloat(changeDue).toFixed(2);
+    if (changeDue >= denominations[idx] && changeDue > 0) {
+      let count = 0;
+      let cashValue = reversedCid[idx][1];
+      while (cashValue > 0 && changeDue >= denominations[idx]) {
+        cashValue -= denominations[idx];
+        changeDue = parseFloat((changeDue - denominations[idx]).toFixed(2));
         count++;
+        console.log(changeDue);
       }
       if (count > 0) {
-        result.change.push([reversedCid[i][0], count * denominations[i]]);
+        result.change.push([reversedCid[idx][0], count * denominations[idx]]);
       }
     }
-    if (changeDue > 0) {
-      return (displayChangeDue.innerHTML = "<p>Status: INSUFFICIENT_FUNDS</p>");
-    }
+    console.log(changeDue);
   }
+  if (changeDue > 0) {
+    return (displayChangeDue.innerHTML = "<p>Status: INSUFFICIENT_FUNDS2</p>");
+  }
+  formatOutput(result.status, result.change);
 };
 
+const formatOutput = (status, change) => {
+  displayChangeDue.innerHTML = `<p>Status: ${status}</p>`;
+  change.map(
+    (cash) => (displayChangeDue.innerHTML += `<p>${cash[0]}: $${cash[1]}</p>`)
+  );
+  return;
+};
 const checkResults = () => {
   if (!cash.value) {
     return alert("Enter amount");
   }
-  checkCashRegister();
+  checkRegister();
 };
+
+cash.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    checkResults();
+  }
+});
+
+purchaseBtn.addEventListener("click", checkResults);
