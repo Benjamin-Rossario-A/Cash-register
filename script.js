9f8fd574669e78b714c2a99d9c89ea466b42d596
Benjamin Rossario A <benrobotics74@gmail.com>
2024-07-16 23:26:10 +0530

    CheckRegister() Basic case

--- script.js
+++ script.js
@@ -10,3 +10,40 @@ let cid = [
   ["TWENTY", 60],
   ["ONE HUNDRED", 100],
 ];
+const displayChangeDue = document.getElementById("change-due");
+const cash = document.getElementById("cash");
+const purchaseBtn = document.getElementById("purchase-btn");
+const priceScreen = document.getElementById("price-screen");
+const cashDrawerDisplay = document.getElementById("cash-drawer-display");
+
+const checkRegister = () => {
+  if (Number(cash.value) < price) {
+    alert("Customer does not have enough money to purchase the item");
+    cash.value = "";
+    return;
+  }
+  if (Number(cash.value) === price) {
+    displayChangeDue.innerHTML =
+      "<p>No change due - customer paid with exact cash</p>";
+    return;
+  }
+
+  let changeDue = Number(cash.value) - price;
+  let reversedCid = [...cid].reverse();
+  let denominations = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01];
+  let result = { status: "OPEN", change: [] };
+  let totalCid = parseFloat(
+    cid
+      .map((total) => total[1])
+      .reduce((prev, curr) => prev + curr)
+      .toFixed(2)
+  );
+
+  if (totalCid < changeDue) {
+    displayChangeDue.innerHTML = "<p>Status: INSUFFICIENT_FUNDS</p>";
+    return;
+  }
+  if (totalCid === changeDue) {
+    result.status = "CLOSED";
+  }
+};
